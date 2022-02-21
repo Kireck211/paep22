199f9009ee7579f1f19fda81dcb388488cbbe5b0
Kireck211 <erickdesantiago11@gmail.com>
2022-02-21 06:51:45 -0600

    feat: Sesion 10 Feb 21

--- sesion10/src/app.js
+++ sesion10/src/app.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const path = require('path');
 
 // const logger = require('./middlewares/logger');
-const swaggerDocument = YAML.load(path.resolve('./src/docs/swagger.yaml'));
 const users = require('./routes/users');
 const locations = require('./routes/locations');
 const {NotFoundError} = require('./utils/errors');
@@ -13,7 +9,6 @@ const app = express();
 
 app.use(express.json());
 // app.use(logger());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
